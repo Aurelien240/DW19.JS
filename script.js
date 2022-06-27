fea8993d2f7aacedccfc6abfe0b41db094f438ca
Aurelien <aureliencolliaux@gmail.com>
2022-06-27 14:26:21 +0200

    first commit

--- script.js
+++ script.js
@@ -5,4 +5,32 @@
         3. Boucles
         4. Fonctions
         */
-        console.error("test");
+
+        /*
+        1. Pourquoi ? Stocker des données EX: Nom d'un jeu ="jeu du plus ou du moins"; 
+           Une Variable: le nom de la var et sa valeur.
+           Le numérique ne comporte jamais de "".
+           Les chaines de caractères contiennent des "".
+           Additionnés des nombres possible
+           Additionnés une chaine de caractère avec autre chose fait de la concatenation.
+        */
+       a=10
+       b=50
+       c="80"
+       b=90
+       console.error(a+parseInt(c));
+       console.error(typeof(c));
+       console.error(b);
+
+
+       SEL=100
+       SUCRE=80
+       console.error("Nous avons du SEL en Q. de " + SEL + "Nous avons du sucre en quantité de "+ SUCRE")
+       
+       // Inversé les variables
+
+       SEL=80
+       SUCRE=100
+       console.error("Nous avons du SEL en Q. de " + SEL + "Nous avons du sucre en quantité de "+ SUCRE")
+
+
