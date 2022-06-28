85fda6cf236d7eb39308ce7e90ebdddadaa00135
Aurelien <aureliencolliaux@gmail.com>
2022-06-28 11:58:08 +0200

    first commit

---
+++ script3.js
@@ -0,0 +1,39 @@
+/* Boucles
+
+    Répéter une action de façon définie ou indéfinie
+
+*/
+//Boucle for qui initialise un compteur nommer i qui démarre de 0 et termine a 5
+/*for (i=0;i<5;i++){
+    alert("test")
+}*/
+
+/*for (i=0;i<100;i++){
+    console.log(i)
+}
+
+for (x=100;x<201;x++){
+    console.log(x)
+}
+
+for (z=1;z<11;z++){
+    result=z*2
+    console.log("2*"+z+"="+result)
+}*/
+
+/*for (i=0;i<10;i++){
+    console.log("boucle "+i)
+
+    for (j=0;j<11;j++){
+        result=j*i
+        console.log(j+"*"+i+"="+result)
+    }
+}*/
+for (i=0;i<11;i++){
+    console.log("boucle "+i)
+
+    for (j=0;j<11;j++){
+        result=j+i
+        console.log(j+"+"+i+"="+result)
+    }
+}
