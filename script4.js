dcb16cb26fef160d2a071dcad4c5ff585dc27093
Aurelien <aureliencolliaux@gmail.com>
2022-06-28 14:11:02 +0200

    first commit

--- script4.js
+++ script4.js
@@ -3,8 +3,19 @@
 
 */
 //Création de la fonction
-function mafonction(){
+/*function mafonction(){
     alert("Hello World")
 }
 //Appel
-mafonction()
+mafonction()*/
+
+function mafonction1(){
+a=prompt("Quel age avez vous")
+console.log("L'utilisateur a "+a+" ans")
+b=parseInt(a) + 2
+console.log("L'utilisateur aura "+b+ " ans ");
+if (a == 25){
+    console.log("L'utilisateur aura 27 ans dans 2 ans")
+}
+}
+mafonction1()
