42272eb70f48d308b97cc9e573caa9070c85d208
Aurelien <aureliencolliaux@gmail.com>
2022-06-28 09:59:58 +0200

    first commit

---
+++ script2.js
@@ -0,0 +1,41 @@
+//Conditions
+
+/*
+    if
+
+    else
+
+    then
+*/ 
+
+/*age=prompt("Mettez votre âge");
+age=parseInt(age)
+
+if (age>18){
+    console.log("Vous êtes majeur")
+}
+if (age<18){
+    console.log("Vous êtes mineur")
+}
+//==tester l'égalité de la valeur entre deux infos
+//===tester l'égalité de la valeur et l'égalité de type entre deux infos
+if (age==18){ 
+    console.log("Vous êtes pile majeur")
+}*/
+
+n=prompt("Choississez un nombre")
+n=parseInt(n)
+
+if (n>0){
+    console.log("le nombre est positif")
+}
+if (n<0){
+    console.log("le nombre est négatif")
+}
+
+if(n%2 == 0){
+    console.log("le nombre est pair")
+}
+else{
+    console.log("le nombre est impair")
+}
