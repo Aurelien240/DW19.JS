ddb4adc2c8d1e51797c858bbf0be2ec14b6f9dde
Aurelien <aureliencolliaux@gmail.com>
2022-06-28 14:49:37 +0200

    first commit

--- script4.js
+++ script4.js
@@ -9,13 +9,49 @@
 //Appel
 mafonction()*/
 
-function mafonction1(){
-a=prompt("Quel age avez vous")
-console.log("L'utilisateur a "+a+" ans")
-b=parseInt(a) + 2
-console.log("L'utilisateur aura "+b+ " ans ");
+/*function mafonction1(){
+
 if (a == 25){
+    a=prompt("Choisissez un nombre")
+console.log(a)
+b=parseInt(a) + 2
+console.log(b);
     console.log("L'utilisateur aura 27 ans dans 2 ans")
 }
 }
-mafonction1()
+mafonction1()*/
+
+//fonction avec paramètres
+/*function afficher(prenom){
+//description
+console.log("Bonjour "+ prenom)
+}
+//appel
+afficher("aurelien")
+afficher("nicolas")
+afficher("axel")*/
+function ecrire(nombre){
+    a=prompt("Choisissez un nombre")
+    b=parseInt(a) + 2
+    console.log(b)
+}
+ecrire()
+
+function num(nombre){
+    if (nombre>0){
+        console.log("Le nombre est positif")
+    }
+    else{
+        console.log("Le nombre est négatif")
+    }
+}
+
+function pair(nombre){
+    if{
+        ((nombre % 2 ) ==0 )
+        console.log("Le nombre est pair")
+    }
+    else{
+        console.log("Le nombre est impair")
+    }
+}
