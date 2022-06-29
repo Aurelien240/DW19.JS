afb4c7072b92d312bbcda2a590449ea22e62c302
Aurelien <aureliencolliaux@gmail.com>
2022-06-29 13:35:43 +0200

    first commit

--- script5.js
+++ script5.js
@@ -27,16 +27,52 @@
     document.getElementById("madiv").innerHTML="test"
  }*/
 
- function ecrire(){
-    document.getElementById("madiv").innerHTML=document.getElementById("texte").value
- }
+//  function ecrire(){
+//     document.getElementById("madiv").innerHTML=document.getElementById("texte").value
+//  }
  
-function boutton(){
-   compteur=parseInt(document.getElementById("madiv2").innerHTML) + 1
-   
-   document.getElementById("madiv2").innerHTML=compteur;
- }
+//   // document.getElementById("compteur").addEventListener( "click" , boutton  )
 
- function majeur(){
+//  function boutton(){
+//    compteur=parseInt(document.getElementById("madiv2").innerHTML) + 1
+//    document.getElementById("madiv2").innerHTML=compteur
+//  }
 
- }
+//  function majeur(){
+//    age=parseInt(document.getElementById("age").value)
+
+// if (age>18){
+//     console.log("Vous êtes majeur")
+// }
+
+// else{
+//    console.log("Vous êtes mineur")
+// }
+// }
+
+document.getElementById("multiplication_boutton").addEventListener("click", multiplication_fonction)
+
+function multuplication_fonction(){
+    // 1. RECUPERER CE QUI A DANS L INPUT TEXTE  
+    numero=parseInt(document.getElementById("multiplication_texte").value)
+    console.error(numero)
+
+
+
+    // 2. MODIFIER LA DIV
+    document.getElementById("multiplication_div").innerHTML="Table de " + numero
+ 
+    // 3. MULTIPLICATION AVEC LA BOUCLE FOR
+    for (i=1;i<10;i++){
+        document.getElementById("multiplication_div").innerHTML+="<br />" + numero+" * "+ i + " = " + (numero * i ) 
+        console.error(i)
+    }
+    
+
+}
+/**
+ * EXO 1 : LES IF ?
+ * EXO 2 : CREEZ UN CHAMP TEXTE, UN BOUTON, UNE DIV 
+ * SI J ECRIS DANS LE CHAMPS TEXTE LE NOMBRE 5
+ * JE VAIS AVOIR LE RESULTAT 1 + 2 + 3 + 4 + 5 = 
+ */
