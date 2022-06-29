cf63feaa99b5b1719ac0debe67bf7393c8919018
Aurelien <aureliencolliaux@gmail.com>
2022-06-29 15:41:12 +0200

    first commit

--- script6.js
+++ script6.js
@@ -6,3 +6,18 @@ console.log(result)
 }
 
 moyenne(15,17)
+
+let tab = [12,13,14,10,12];
+console.log(tab[2])
+console.table(tab) 
+
+
+function moyenneindef(tab){
+  let result=0
+  for (let i=0; i<tab.length ; i++   ){
+      result=result+tab[i]
+  }
+  result=result/tab.length
+  return result
+}
+console.log(moyenneindef([12,14]))
