d6916e27dbf72e84c7dd3b1ab2687bc076864764
Aurelien <aureliencolliaux@gmail.com>
2022-07-01 14:29:17 +0200

    first commit

--- script7.js
+++ script7.js
@@ -1,7 +1,17 @@
-document.getElementById("SOMME_boutton").addEventListener("click", change)
-function change(){
 
-    document.getElementById("body").style.background = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+function entierAleatoire(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+   }
+   let tab=["yellow","green","blue","red"]
+
+
+function couleuraleatoire(tableau){
+
 }
 
 
+function change(){
+
+    // document.getElementById("body").style.background = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+}
+document.getElementById("SOMME_boutton").addEventListener("click", change)
