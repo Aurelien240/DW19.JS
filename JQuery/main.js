1211658f1cc0e0fba032e1cb26a0940e81742506
Aurelien <aureliencolliaux@gmail.com>
2022-07-18 10:00:27 +0200

    first commit

---
+++ JQuery/main.js
@@ -0,0 +1,21 @@
+// JQuery est une librairie Javascript
+// Utilisée pour faciliter la manip du DOM
+
+// Sélectioner des éléments
+// ------------------------
+
+// Sélectioner en JS
+// const title = document.querySelector ('h1')
+
+// Sélectioner un élément en JQuery
+// const title = $('h1')
+
+// Sélectioner des éléments en JQuery
+// ATTENTION par défaut JS sélectionne le premier élément qu'il trouve
+// $("button") => nous trouve tout les boutons sur notre page HTML
+
+// ------------------------
+// Modifier du texte
+// ------------------------
+
+ $('h1').text("Bonjour les DW 19 !!");
