830711a3eabcf9a5fa5b7963caf009c7f2d43658
Aurelien <aureliencolliaux@gmail.com>
2022-07-18 10:20:22 +0200

    first commit

--- JQuery/main.js
+++ JQuery/main.js
@@ -18,4 +18,25 @@
 // Modifier du texte
 // ------------------------
 
- $('h1').text("Bonjour les DW 19 !!");
+// $('h1').text("Bonjour les DW 19 !!");
+
+// ------------------------
+// Modifier le HTML
+// ------------------------
+
+// $("div").html("Je suis le nouveau contenu HTML");
+
+// ------------------------
+// Les template strings
+// ------------------------
+
+// 1. Elle vous permet d'écrire du code sur plusieurs lignes (sans faire de concaténation)
+// 2. Elle permet d'inclure du JS à l'intérieur d'une string
+
+const firstname = "Aurélien";
+
+$("div").html(`
+<h2>Bonjour $(firstname)</h2>
+<h4>les DW 19</h4>
+<p>blablablabla....</p>
+`);
